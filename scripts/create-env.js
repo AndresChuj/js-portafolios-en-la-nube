1157296ee33be121c84a272cbda95e7afbdac136
pablo chuj <chuj2019@outlook.es>
2022-08-17 16:45:26 -0600

    solucionando error

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,4 +1,4 @@
 //para trabajar con un modulo de node que nos permite trabajar con el sistema operativo y nos permite guardar o enviar datos
 const fs = require('fs');
 
-fs.writeFileSync('./.env', `API=${process.env.API}/n`)
+fs.writeFileSync('./.env', `API=${process.env.API}\n`)
